8f27f54a52ba546b678b2ad9a05276ea779079e9
Renzobor <renzo-ejem@hotmail.com>
2024-09-04 12:45:01 -0300

    4/9/2024

--- Book practices/the-c-programming-languaje/1.22.c
+++ Book practices/the-c-programming-languaje/1.22.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 void main(){
-    const int COLUMN = 15;
+    
+    const int COLUMN = 20;
     int c, i, charCount = 0, columnCount = 0;
     char word[100];
     word[0] = '\0';
-    while((c=getchar()) != '\n'){
-
+    while((c=getchar()) != EOF ){
         word[charCount] = c;
         word[charCount + 1] = '\0';
         charCount++;
-        printf("%s\n",word);
+        columnCount++;
+        if (columnCount == COLUMN){
+            printf("\n");
+            columnCount = 0;
+        }
+        if(c == ' ' || c == '\n'){
+            printf("%s", word);
+            charCount = 0;
+
+        };
+        if(strlen(word) == COLUMN - 1){
+            printf("%s", word);
+            printf("-\n");
+            columnCount = 0;
+            charCount = 0;
+            word[0] = '\0';
+        } 
 
     }
 }
